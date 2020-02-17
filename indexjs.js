ab6b3c6112f3a6199c3f47fd9af193f650cf4752
kiilkim <58096214+kiilkim@users.noreply.github.com>
2020-02-17 20:21:15 +0900

    0217

---
+++ indexjs.js
@@ -0,0 +1,58 @@
+alert('코딩 복습하러 오신것을 환영합니다 _기일')
+
+//첫화면 html 넘어가기 
+function html_next() {
+    prompt("html 공부 파이팅!","html 공부의 목적은?");
+}
+
+/* 첫화면 자바 넘어가기 */
+function java_st() {
+    window.location="java/javacla12.html"
+}
+
+
+function java_ask() {
+    prompt("java공부의 목적은?");
+}
+
+/* 첫화면 JSP 넘어가기 */
+function jsp_st() {
+    window.location="jspcla12.html";
+}
+
+
+function jsp_ask() {
+    prompt("jsp공부의 목적은?");
+}
+
+function db_st() {
+    window.location="dbcla01.html";
+}
+
+
+function db_ask() {
+    prompt("db공부의 목적은?");
+}
+
+
+/* function myMove() {
+    var elem = document.getElementById("aniImg");
+    var pos = 0;
+    var id =setInterval(frame, 10);
+
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+
+            pos++;
+            elem.style.top = pos+'px';
+            elem.stylee.left = pos+'px';
+
+        }
+
+
+    }
+
+
+} */
